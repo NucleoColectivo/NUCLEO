7243c915c80a45d4dc4868e98e76d7de068c0338
NucleoColectivo <nucleo.colectivo.art@gmail.com>
2026-01-15 03:51:36 -0500

    Updated main.tsx

--- nucleo-colectivo/src/main.tsx
+++ nucleo-colectivo/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LanguageProvider } from './contexts/LanguageContext.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
 import './index.css'
 import App from './App.tsx'
@@ -7,7 +8,9 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
