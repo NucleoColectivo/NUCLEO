44f8e615c42ba9cd26277d8f9aff77a8f88dce56
NucleoColectivo <nucleo.colectivo.art@gmail.com>
2026-01-15 16:41:37 -0500

    Updated App.tsx

--- nucleo-colectivo/src/App.tsx
+++ nucleo-colectivo/src/App.tsx
@@ -2,16 +2,13 @@ import HeaderNavigation from './components/HeaderNavigation';
 import HomeManifiesto from './components/HomeManifiesto';
 import EstadisticasImpacto from './components/EstadisticasImpacto';
 import MiembrosSection from './components/MiembrosSection';
-import ArtistsGallery from './components/ArtistsGallery';
 import NucleoSection from './components/NucleoSection';
 import ObraSection from './components/ObraSection';
 import GaleriaSection from './components/GaleriaSection';
 import EducacionSection from './components/EducacionSection';
-import CalculadoraTalleres from './components/CalculadoraTalleres';
 import LaboratorioCreativo from './components/LaboratorioCreativo';
 import ProyectosEspecialesSection from './components/ProyectosEspecialesSection';
 import ContenidosSection from './components/ContenidosSection';
-import NewsletterLeadMagnet from './components/NewsletterLeadMagnet';
 import FAQSection from './components/FAQSection';
 import ContactoAlianzasSection from './components/ContactoAlianzasSection';
 import BibliotecaRecursos from './components/BibliotecaRecursos';
@@ -25,16 +22,13 @@ function App() {
         <HomeManifiesto />
         <EstadisticasImpacto />
         <MiembrosSection />
-        <ArtistsGallery />
         <NucleoSection />
         <ObraSection />
         <GaleriaSection />
         <EducacionSection />
-        <CalculadoraTalleres />
         <LaboratorioCreativo />
         <ProyectosEspecialesSection />
         <ContenidosSection />
-        <NewsletterLeadMagnet />
         <FAQSection />
         <ContactoAlianzasSection />
       </main>
